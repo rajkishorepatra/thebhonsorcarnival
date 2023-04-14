d63f5fc01acb2da02b6aaf745e5f2566ce89188c
rajkishorepatra <rpatrasm@gmail.com>
2023-04-14 10:50:52 +0530

    All changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+import { React, useEffect } from "react";
 import './App.css';
+import logo from './img/logo.png'
+import Tilt from 'react-parallax-tilt';
+import ConfettiGenerator from "confetti-js";
+import avatar from './img/swagbabu.png';
+
 
 function App() {
+  useEffect(() => {
+    const confettiSettings = {
+      target: "my-canvas",
+      respawn: false,
+      max: 400,
+      clock: 20
+    };
+    const confetti = new ConfettiGenerator(confettiSettings);
+    setTimeout(() => {
+      confetti.render();
+    }, 0);
+
+    return () => confetti.clear();
+  }, []);
+  document.body.style.overflowX = "hidden";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <canvas
+        id="my-canvas"
+        style={{ position: "absolute" }}
+      ></canvas>
+      <div className='container'>
+        <Tilt>
+          <img className='demo-img' src={logo} style={{ cursor: "pointer" }} alt="logo" />
+        </Tilt>
+      </div>
+      <h2>Welcome to the first edition of The Bhonsor Carnival</h2>
+
+
+      <div className='subscribe'>
+        <h1>Lorem ipsum is a placeholder text</h1>
+        <div class="custom-search">
+          <input type="text" class="custom-search-input" placeholder="Enter Your Whatsapp Number" required />
+          <button class="custom-search-botton" type="submit">Subscribe</button>
+        </div>
+
+        <div className="avatar">
+          <img src={avatar} alt="avatar" />
+        </div>
+      </div>
+
+      <div className="footer">
+        <p>©The Bhonsor Carnival 2023</p>
+      </div>
+    </>
   );
 }
 
