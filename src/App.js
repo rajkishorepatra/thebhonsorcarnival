85db83f9c7c0cd48f0fc6daa2017690de14a5ea5
rajkishorepatra <rpatrasm@gmail.com>
2023-04-14 16:02:40 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { React, useEffect } from "react";
+import { React, useEffect, useState, useRef } from "react";
 import './App.css';
-import logo from './img/logo.png'
 import Tilt from 'react-parallax-tilt';
 import ConfettiGenerator from "confetti-js";
-import avatar from './img/swagbabu.png';
 
 
 function App() {
@@ -12,7 +10,7 @@ function App() {
       target: "my-canvas",
       respawn: false,
       max: 400,
-      clock: 20
+      clock: 40
     };
     const confetti = new ConfettiGenerator(confettiSettings);
     setTimeout(() => {
@@ -22,6 +20,24 @@ function App() {
     return () => confetti.clear();
   }, []);
   document.body.style.overflowX = "hidden";
+
+  const [num, setNum] = useState('');
+  const formRef = useRef(null)
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbwgq-Izm8OXw1dDTeQgWjhgiEKrv4UQXHkVYyNAg_Ap0r9f1JgO-jY0wpqdxtakWK1ehQ/exec"
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(scriptUrl, {
+      method: 'POST',
+      'mode': 'no-cors',
+      body: new FormData(formRef.current),
+
+    }).then(res => {
+      console.log(setNum())
+    })
+      .catch(err => console.log(err));
+    setNum('');
+  }
   return (
     <>
       <canvas
@@ -30,21 +46,30 @@ function App() {
       ></canvas>
       <div className='container'>
         <Tilt>
-          <img className='demo-img' src={logo} style={{ cursor: "pointer" }} alt="logo" />
+          <img className='demo-img' src="https://ik.imagekit.io/dqe4fvjcky/logo_MIJa25Rc6t.png?updatedAt=1681467998214" style={{ cursor: "pointer" }} alt="logo" />
         </Tilt>
       </div>
       <h2>Welcome to the first edition of The Bhonsor Carnival</h2>
 
 
       <div className='subscribe'>
-        <h1>Lorem ipsum is a placeholder text</h1>
-        <div class="custom-search">
-          <input type="text" class="custom-search-input" placeholder="Enter Your Whatsapp Number" required />
-          <button class="custom-search-botton" type="submit">Subscribe</button>
-        </div>
+        <h1>Kindly Enter Your Phone Number</h1>
+        <form onSubmit={handleSubmit} method='post' ref={formRef} name="google-sheet">
+          <div class="custom-search">
+            <input 
+              type="text" 
+              class="custom-search-input" 
+              placeholder="Enter Your Whatsapp Number" 
+              name='Whatsapp-Number'
+              onChange={(e) => setNum(e.target.value)}
+              value={num} 
+              required />
+            <button class="custom-search-botton" type="submit" style={{cursor:"pointer", fontSize:"1.1em", color:"black"}}>Submit</button>
+          </div>
+        </form>
 
         <div className="avatar">
-          <img src={avatar} alt="avatar" />
+          <img src="https://ik.imagekit.io/dqe4fvjcky/swagbabu-min_h3wr2hr9Z.png?updatedAt=1681468107228" alt="avatar" />
         </div>
       </div>
 
